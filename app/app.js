01571592056fb82e1889fe362b56eaccf4b0f368
acary <andy@andycary.com>
2017-02-02 16:30:34 -0800

    added pages to learn controllers and services

--- app/app.js
+++ app/app.js
@@ -12,3 +12,48 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
 
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+// Controllers (index-controllers.html)
+angular.module('app', []);
+
+angular.module('app').controller('MainCtrl', function (){
+  var self = this;
+  self.message = 'hello';
+  console.log(self.message);
+
+  self.changeMessage = function(message) {
+    self.message = message;
+  };
+});
+
+// Services (index-services.html)
+angular.module('app').factory('messages', function() {
+  var messages = {};
+
+  messages.list = [];
+  messages.add = function(message) {
+    messages.list.push({id: messages.list.length, text: message});
+  };
+
+  return messages;
+});
+
+// Controller for Services
+angular.module('app').controller('ListCtrl', function(messages) {
+  var self = this;
+
+  self.messages = messages.list;
+
+});
+
+// Controller for updating messages
+angular.module('app').controller('PostCtrl', function(messages) {
+  var self = this;
+  self.message = "Hello, great World!";
+
+  self.addMessage = function(message) {
+    messages.add(message);
+    self.newMessage = '';
+  };
+
+});
